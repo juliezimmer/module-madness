f3d0e542e42520c97f749fdf1f79081912f67b15
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2017-08-04 16:12:09 -0500

    added formatting for account balance and number

--- moduleThree.js
+++ moduleThree.js
@@ -4,17 +4,16 @@ var random = require("./randomNumber.js");
 var finalBalance = accountBalance(random(100, 1000000));
  
 
-console.log(finalBalance);
+//console.log(finalBalance);
 
 function findBalance(accountBalance, random) {
     return(accoutBalance(random));
 }
     
-//var accountBalance =  
 
 module.exports = ({ 
     accountBalance: findBalance,
-    name: "Account balance:" + finalBalance
+    name: "Account balance:" + "\n" + finalBalance
 });
  console.log(module.exports.name); 
 
